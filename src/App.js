f871551965e0026dead4476a4cbee46484ca1af6
Shubham Kushwaha <eric.liclair@gmail.com>
2021-12-15 11:56:56 +0530

    removing errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Button, CircularProgress, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import { Box } from '@mui/system';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,7 +18,13 @@ function App() {
   const [name, setName] = useState(form.name);
   const [age, setAge] = useState(form.age);
   const [gender, setGender] = useState(form.gender);
+  const clearForm = () => {
+    setAge(null);
+    setName(null);
+    setGender(null);
+  }
 
+  const [loading, setLoading] = useState(false);
 
   const changeHandler = (type, e) => {
     // form submission handler
@@ -47,35 +53,48 @@ function App() {
   }
 
   const handleSubmission = () => {
-
+    // form submission
+    setLoading(true);
+    dispatch(updateForm({
+      name: null,
+      age: null,
+      gender: null
+    }))
+    clearForm();
+    setTimeout(() => setLoading(false), 3000);
+    // usedispatch
   }
 
   return (
     <Box sx={{ maxWidth: 350, padding: '1rem', backgroundColor: 'palette.background.secondary' }}>
-      <ElementWrap>
-        <TextField label='Name' id='name' value={name} onChange={(e) => changeHandler('NAME', e)} />
-      </ElementWrap>
-      <ElementWrap>
-        <FormControl>
-          <InputLabel id="gender">Gender</InputLabel>
-          <Select
-            labelId="gender"
-            id="gender"
-            value={gender}
-            label="Gender"
-            onChange={(e) => changeHandler('GENDER', e)}
-          >
-            <MenuItem value={'MALE'}>Male</MenuItem>
-            <MenuItem value={'FEMALE'}>Female</MenuItem>
-          </Select>
-        </FormControl>
-      </ElementWrap>
-      <ElementWrap>
-        <TextField value={age} type={'number'} label='Age' id='age' onChange={(e) => changeHandler('AGE', e)} />
-      </ElementWrap>
-      <ElementWrap>
-        <Button onClick={(clickHandler) => { }}>Submit Form</Button>
-      </ElementWrap>
+      {
+        loading ? <CircularProgress size={36} /> : <Box>
+          <ElementWrap>
+            <TextField label='Name' id='name' value={name} onChange={(e) => changeHandler('NAME', e)} />
+          </ElementWrap>
+          <ElementWrap sx={{ width: '100%' }}>
+            <FormControl>
+              <InputLabel id="gender">Gender</InputLabel>
+              <Select
+                labelId="gender"
+                id="gender"
+                value={gender || 'MALE'}
+                label="Gender"
+                onChange={(e) => changeHandler('GENDER', e)}
+              >
+                <MenuItem value={'MALE'}>Male</MenuItem>
+                <MenuItem value={'FEMALE'}>Female</MenuItem>
+              </Select>
+            </FormControl>
+          </ElementWrap>
+          <ElementWrap>
+            <TextField value={age} type={'number'} label='Age' id='age' onChange={(e) => changeHandler('AGE', e)} />
+          </ElementWrap>
+          <ElementWrap>
+            <Button onClick={handleSubmission}>Submit Form</Button>
+          </ElementWrap>
+        </Box>
+      }
     </Box>
   );
 }
